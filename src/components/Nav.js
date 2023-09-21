cbf138c8d1e602474b8c04fa82d1fbc3658cd5ff
hansg0325 <hansg0325@gmail.com>
2023-09-21 13:03:25 +0900

    react-netflix 배포

---
+++ src/components/Nav.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+import { useNavigate } from "react-router-dom";
+
+export default function Nav() {
+  const [show, setshow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        setshow(true);
+      } else {
+        setshow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
+  return (
+    <nav className={`nav ${show && "nav__black"}`}>
+      <img
+        alt="Netflix logo"
+        src="//upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요."
+      />
+      <img
+        alt="User logged"
+        src="https://occ-0-993-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdYJV5wt63AcxNaDoqDXUhqZb55oN5Dxt1m-Zdn_z5rn_hIq9m8dA8JB2xdcPmrY3yXnlVWYKPXnOrbv2QN4aEVU28dESJg.png?r=1d4"
+        className="nav__avatar"
+      />
+    </nav>
+  );
+}
